5f4ee75965b60a1c2b6b6e1678117f3e8a48079f
huongdv22 <110890611+huongdv22@users.noreply.github.com>
2022-08-23 17:32:36 +0700

    Revert "push"
    This reverts commit edaa2d7588b1a23417abcdf5d6a257aef6dfe445.

--- lib/utils/data/enum_utils.dart
+++
@@ -1,9 +0,0 @@
-import 'package:collection/collection.dart';
-
-class EnumUtils {
-  static T? enumFromString<T>(Iterable<T?> values, String? value) {
-    return values
-        .firstWhereOrNull((type) => type.toString().split('.').last == value);
-
-  }
-}
--- lib/utils/helpers/system_utils.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:developer';
-
-import 'package:flutter/foundation.dart';
-
-class SystemUtils {
-  static debugLog(String source, String message) {
-    if (kDebugMode) log(message, name: source);
-  }
-}
